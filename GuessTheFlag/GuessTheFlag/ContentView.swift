5ec97ce29bd7932a1036e4c8b1cb83f8b713155b
queuebit <queuebit10648@gmail.com>
2020-05-03 17:34:14 -0400

    Gradients <https://www.hackingwithswift.com/books/ios-swiftui/gradients>

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,7 +9,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    let gridSpacing : CGFloat = 70
+    let gridSpacing: CGFloat = 70
+    let gradientWidth:  CGFloat = 70
+    let gradientHeight:  CGFloat = 70
 
     var body: some View {
         VStack(spacing: gridSpacing) {
@@ -29,8 +31,13 @@ struct ContentView: View {
                 Text("8")
                 Text("9")
             }
+            HStack(spacing: gridSpacing / 2) {
+                LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .top, endPoint: .bottom).frame(width: gradientWidth, height: gradientHeight)
+                RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 5, endRadius: 75).frame(width: gradientWidth, height: gradientHeight)
+                AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center).frame(width: gradientWidth, height: gradientHeight)
+            }
+
         }
-        .background(Color(red: 1, green: 0.8, blue: 0))
     }
 }
 
