c4374c59bb369d512c8207f50c47157fcb34e712
queuebit <queuebit10648@gmail.com>
2020-05-02 22:18:17 -0400

    Finished with v0 of COVID Cruise. - Calculates cases, asymptomatic, ICU admits and deaths based on CDC rates of Diamond Princess.

--- COVIDCruise/COVIDCruise/ContentView.swift
+++ COVIDCruise/COVIDCruise/ContentView.swift
@@ -9,16 +9,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var numberOfPassengers = ""
+
+    let infectionRate = 0.192
+    let aPreRate = 0.465
+    let icuRate = 0.097
+    let deathRate = 0.013
+    let passengerToCrewRatio: Double = 2.55
+    var numberOfCrew: Int {
+        return Int((Double(numberOfPassengers) ?? 0) / passengerToCrewRatio)
+    }
+    var totalOnBoard: Int {
+        return (Int(numberOfPassengers) ?? 0) + numberOfCrew
+    }
+
+    var covidPositive: Int {
+        return Int(Double(totalOnBoard) * infectionRate)
+    }
+    var aPreSymptomatic: Int {
+        return Int(Double(covidPositive) * aPreRate)
+    }
+    var icuAdmits: Int {
+        return Int(Double(covidPositive) * icuRate)
+    }
+    var deaths: Int {
+        return Int(Double(covidPositive) * deathRate)
+    }
+
     var body: some View {
         NavigationView {
             Form {
-                Section {
-                    Text("Input")
+                Section(header: Text("Inputs")) {
+                    TextField("Number of passengers", text: $numberOfPassengers)
+                        .keyboardType(.numberPad)
+
+                    Text("\(numberOfCrew) crew")
+                    Text("\(totalOnBoard) total on board")
                     
                 }
                 
-                Section {
-                    Text("Output")
+                Section(header: Button(action: {
+                    UIApplication.shared.open(URL(string: "https://www.cdc.gov/mmwr/volumes/69/wr/mm6912e3.htm#diamondprincess")!)
+                }) { Text("Rates calculated from Diamond Princess: CDC")} ) {
+                    Text("Cases: \(covidPositive)")
+                    Text("A|Pre-symptomatic: \(aPreSymptomatic)")
+                    Text("ICU Admits: \(icuAdmits)")
+                    Text("Deaths: \(deaths)")
                 }
             }
         .navigationBarTitle("COVID-19 Cruise")
