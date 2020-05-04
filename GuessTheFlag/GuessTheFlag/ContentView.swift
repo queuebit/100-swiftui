fbe83b23561aadb47ea1b58b62d562f1c87094ee
queuebit <queuebit10648@gmail.com>
2020-05-04 18:19:32 -0400

    Styling our flags. <https://www.hackingwithswift.com/books/ios-swiftui/styling-our-flags>

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -16,7 +16,8 @@ struct ContentView: View {
 
     var body: some View {
         ZStack {
-            Color.blue.edgesIgnoringSafeArea(.all)
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
 
             VStack(spacing: 30) {
                 VStack {
@@ -25,6 +26,8 @@ struct ContentView: View {
 
                     Text(countries[correctAnswer])
                         .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
                 }
 
                 ForEach(0 ..< 3) { number in
@@ -33,6 +36,10 @@ struct ContentView: View {
                     }) {
                         Image(self.countries[number])
                             .renderingMode(.original)
+                        .clipShape(Capsule())
+                            .overlay(Capsule()
+                                .stroke(Color.black, lineWidth: 1))
+                            .shadow(color: .black, radius: 2)
                     }
                 }
 
