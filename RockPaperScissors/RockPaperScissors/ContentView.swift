792407838480f5f87410beedf55ccc8818ffa2dc
queuebit <queuebit10648@gmail.com>
2020-05-08 20:17:04 -0400

    v0 of the data for RockPaperScissors. <https://www.hackingwithswift.com/guide/ios-swiftui/2/3/challenge>

--- RockPaperScissors/RockPaperScissors/ContentView.swift
+++ RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -9,8 +9,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    let throwOptions = ["Rock", "Paper", "Scissors"]
+
+    @State private var humanShouldWin = Bool.random()
+    @State private var robotThrows = Int.random(in: 0 ... 2)
+    @State private var playerScore = 0
+    @State private var round = 0
+
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Human should \(humanShouldWin ? "WIN" : "LOSE")")
+            Text("Robot threw \(throwOptions[robotThrows])")
+            Text("Choose your move:")
+            ForEach(throwOptions, id: \.self) {
+                Text($0)
+            }
+            Text("ROUND: \(round)")
+        }
     }
 }
 
