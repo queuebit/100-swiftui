c1a04ee3d6d3ffb88e13cb8581e834c7a4097f9c
queuebit <queuebit10648@gmail.com>
2020-05-10 16:02:29 -0400

    Building a basic layout <https://www.hackingwithswift.com/books/ios-swiftui/building-a-basic-layout>

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -9,23 +9,47 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
     @State private var wakeUp = Date()
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
 
     var body: some View {
-        let formatter = DateFormatter()
-        formatter.timeStyle = .short
-        let dateString = formatter.string(from: Date())
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
 
-        return NavigationView {
-            Form {
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                .labelsHidden()
+
+                Text("Desired amount of sleep")
+                    .font(.headline)
+
                 Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
                     Text("\(sleepAmount, specifier: "%g") hours")
                 }
-                DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                Text("\(dateString)")
+
+                Text("Daily coffee intake")
+                    .font(.headline)
+
+                Stepper(value: $coffeeAmount, in: 1...20) {
+                    if coffeeAmount == 1 {
+                        Text("1 cup")
+                    } else {
+                        Text("\(coffeeAmount) cups")
+                    }
+                }
             }
+            .navigationBarTitle("BetterRest")
+            .navigationBarItems(trailing:
+                Button(action: calculateBedtime) {
+                    Text("Calculate")
+                }
+            )
         }
+    }
+
+    func calculateBedtime() {
     }
 }
 
