a90639d7737ea44225f724e3141a2fdfbe052284
queuebit <queuebit10648@gmail.com>
2020-05-12 17:43:30 -0400

    Loading resources from your app bundle <https://www.hackingwithswift.com/books/ios-swiftui/loading-resources-from-your-app-bundle>

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -9,13 +9,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    let people = ["Finn", "Leia", "Luke", "Rey"]
-
     var body: some View {
-        List(people, id: \.self) {
-            Text($0)
+        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
+            // we found the file in our bundle
+            if let fileContents = try? String(contentsOf: fileURL) {
+                // we loaded the file into a string
+                print(fileContents)
+            }
         }
-    .listStyle(GroupedListStyle())
+
+        return Text("Hello World")
     }
 }
 
