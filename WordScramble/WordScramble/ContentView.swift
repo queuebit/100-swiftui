911f999fd499217c670be497d5effdca42cc57dd
queuebit <queuebit10648@gmail.com>
2020-05-12 17:38:27 -0400

    Introducing List, your best friend <https://www.hackingwithswift.com/books/ios-swiftui/introducing-list-your-best-friend>

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -9,9 +9,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    let people = ["Finn", "Leia", "Luke", "Rey"]
+
     var body: some View {
-        List(0..<5) {
-            Text("Dynamic row \($0)")
+        List(people, id: \.self) {
+            Text($0)
         }
     .listStyle(GroupedListStyle())
     }
