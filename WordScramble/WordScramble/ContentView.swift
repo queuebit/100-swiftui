ba96eefb13cac24e375a135639f52db31e20e732
queuebit <queuebit10648@gmail.com>
2020-05-13 20:09:17 -0400

    Running code when our app launches <https://www.hackingwithswift.com/books/ios-swiftui/running-code-when-our-app-launches>

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -27,6 +27,7 @@ struct ContentView: View {
                 }
             }
         .navigationBarTitle(rootWord)
+        .onAppear(perform: startGame)
         }
     }
 
@@ -41,6 +42,18 @@ struct ContentView: View {
 
         usedWords.insert(answer, at: 0)
         newWord = ""
+    }
+
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+
+        fatalError("Could not load start.txt from bundle.")
     }
 }
 
