f52bb824541df143060e47d45a639c099b842ae1
queuebit <queuebit10648@gmail.com>
2020-05-14 21:00:07 -0400

    Turn off auto-correct

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -24,6 +24,7 @@ struct ContentView: View {
                 TextField("Enter your word", text: $newWord, onCommit: addNewWord)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .autocapitalization(.none)
+                    .disableAutocorrection(true)
                     .padding()
 
                 List(usedWords, id: \.self) {
