1389d9bdb88df5ea7d938c52057247a155a5ae0d
queuebit <queuebit10648@gmail.com>
2020-05-15 20:29:16 -0400

    Animation: Introduction <https://www.hackingwithswift.com/books/ios-swiftui/animation-introduction>

---
+++ Animations/Animations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Paul Gagnon on 5/15/20.
+//  Copyright © 2020 22q. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
