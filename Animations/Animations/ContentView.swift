df0140dc5485e5215a79266f4128a18bb4f749f2
queuebit <queuebit10648@gmail.com>
2020-05-16 14:01:56 -0400

    Building custom transitions using ViewModifier <https://www.hackingwithswift.com/books/ios-swiftui/building-custom-transitions-using-viewmodifier>

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,6 +8,22 @@
 
 import SwiftUI
 
+struct CornerRotateMonifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor)
+        .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(active: CornerRotateMonifier(amount: -90, anchor: .topLeading), identity: CornerRotateMonifier(amount: 0, anchor: .topLeading))
+    }
+}
+
 struct ContentView: View {
     @State private var isShowingRed = false
 
@@ -23,7 +39,7 @@ struct ContentView: View {
                 Rectangle()
                     .fill(Color.red)
                     .frame(width: 200, height: 200)
-                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+                    .transition(.pivot)
             }
         }
     }
