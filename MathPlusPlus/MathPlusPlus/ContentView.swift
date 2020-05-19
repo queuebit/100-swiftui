30e91ca48db9b98250ac61ba550fd0c50502caa7
queuebit <queuebit10648@gmail.com>
2020-05-18 20:33:33 -0400

    v0 of entering a problem / checking answer

--- MathPlusPlus/MathPlusPlus/ContentView.swift
+++ MathPlusPlus/MathPlusPlus/ContentView.swift
@@ -9,8 +9,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var questionNumber = 1
+    @State private var questions = 4
+    @State private var operation = "*"
+    @State private var operands = [12, 7]
+    @State private var correctAnswer = 84
+    @State private var answer: String = ""
+
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                Form {
+                    Section(header: Text("Question \(questionNumber)")) {
+                        Text("What is \([String(operands[0]), operation, String(operands[1])].joined(separator: " ")) ?")
+                        TextField("The answer is..", text: $answer)
+                            .keyboardType(.numberPad)
+                    }
+                }
+
+                Button("Check") {
+                    // do something
+                    print(self.answer)
+                    print(Int(self.answer) == (self.operands[0] * self.operands[1]))
+                }
+
+                Text("\(questionNumber) of \(questions)")
+                .padding()
+            }
+            .navigationBarTitle("Math++")
+
+        }
     }
 }
 
