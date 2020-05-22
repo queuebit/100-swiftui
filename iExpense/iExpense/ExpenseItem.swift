dddf313550b92b91b54b6adaa9c6e31a06ad7fea
queuebit <queuebit10648@gmail.com>
2020-05-21 22:13:21 -0400

    Building a list we can delete from <https://www.hackingwithswift.com/books/ios-swiftui/building-a-list-we-can-delete-from>

---
+++ iExpense/iExpense/ExpenseItem.swift
@@ -0,0 +1,15 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Paul Gagnon on 5/21/20.
+//  Copyright © 2020 22q. All rights reserved.
+//
+
+import Foundation
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Int
+}
