bad2d83466447cba3db44d6d23afaad57b87631f
queuebit <queuebit10648@gmail.com>
2020-05-28 20:55:00 -0400

    Formatting our mission view <https://www.hackingwithswift.com/books/ios-swiftui/formatting-our-mission-view>

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -13,7 +13,24 @@ struct ContentView: View {
     let missions: [Mission] = Bundle.main.decode("missions.json")
 
     var body: some View {
-        Text("NASA had \(astronauts.count) astronauts for \(missions.count) missions")
+        NavigationView {
+            List(missions) { mission in
+                NavigationLink(destination: Text("Detail View")) {
+                    Image(mission.image)
+                    .resizable()
+                        // .aspectRatio(contentMode: .fit)
+                        .scaledToFit()
+                        .frame(width: 44, height: 44)
+
+                    VStack(alignment: .leading) {
+                        Text(mission.displayName)
+                            .font(.headline)
+                        Text(mission.formattedLaunchDate)
+                    }
+                }
+            }
+        .navigationBarTitle("Moonshot")
+        }
     }
 }
 
