2afa2a9ad4c82b63e9b1fa55ecbb3dd78db7ca17
queuebit <queuebit10648@gmail.com>
2020-06-09 20:42:38 -0400

    Moriarty multiply complex

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -8,45 +8,18 @@
 
 import SwiftUI
 
-struct ColorCyclingCircle: View {
-    var amount = 0.0
-    var steps = 100
-
+struct ContentView: View {
     var body: some View {
         ZStack {
-            ForEach(0..<steps) { value in
-                Circle()
-                    .inset(by: CGFloat(value))
-                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
-                        self.color(for: value, brightness: 1),
-                        self.color(for: value, brightness: 0.5)
-                    ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
-            }
+            Image("moriarty")
+                .resizable()
+                .scaledToFit()
+
+            Rectangle()
+                .fill(Color.blue)
+                .blendMode(.multiply)
         }
-        .drawingGroup()
-    }
-
-    func color(for value: Int, brightness: Double) -> Color {
-        var targetHue = Double(value) / Double(self.steps) + self.amount
-
-        if targetHue > 1 {
-            targetHue -= 1
-        }
-
-        return Color(hue: targetHue, saturation: 1, brightness: brightness)
-    }
-}
-
-struct ContentView: View {
-    @State private var colorCycle = 0.0
-
-    var body: some View {
-        VStack {
-            ColorCyclingCircle(amount: self.colorCycle)
-                .frame(width: 300, height: 300)
-
-            Slider(value: $colorCycle)
-        }
+        .frame(width: 400, height: 500)
     }
 }
 
