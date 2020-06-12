e19828fe30435bc1476e21fd19aa2adb99d86e1b
queuebit <queuebit10648@gmail.com>
2020-06-11 21:38:12 -0400

    Added defaulted variable for gradient start / end points.

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -41,19 +41,59 @@ struct Arrow: Shape {
    }
 }
 
+struct ColorCyclingSquare: View {
+    var amount = 0.0
+    var steps = 100
+    var gradStart: UnitPoint = UnitPoint(x: 0, y: 50)
+    var gradEnd: UnitPoint = UnitPoint(x: 0, y: 75)
+
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Rectangle()
+                    .inset(by: CGFloat(value))
+                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
+                        self.color(for: value, brightness: 1),
+                        self.color(for: value, brightness: 0.5)
+                    ]), startPoint: self.gradStart, endPoint: self.gradEnd), lineWidth: 2)
+            }
+        }
+        .drawingGroup()
+    }
+
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
+
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
 struct ContentView: View {
     @State private var arrowRatio: CGFloat = 0.25
     @State private var insetAmount: CGFloat = 75
+    @State private var colorCycle = 0.0
 
     var body: some View {
-        Arrow(arrowRatio: arrowRatio, insetAmount: insetAmount)
-            .frame(width: 200, height: 300)
-            .onTapGesture {
-                withAnimation {
-                    self.arrowRatio = CGFloat.random(in: 0.1...0.5)
-                    self.insetAmount = CGFloat.random(in: 10...90)
+        VStack {
+            Arrow(arrowRatio: arrowRatio, insetAmount: insetAmount)
+                .frame(width: 200, height: 300)
+                .onTapGesture {
+                    withAnimation {
+                        self.arrowRatio = CGFloat.random(in: 0.1...0.5)
+                        self.insetAmount = CGFloat.random(in: 10...90)
+                    }
                 }
+            VStack {
+                ColorCyclingSquare(amount: self.colorCycle)
+                    .frame(width: 300, height: 300)
+
+                Slider(value: $colorCycle)
             }
+        }
     }
 }
 
